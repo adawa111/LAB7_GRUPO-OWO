22c8167ff4cf0b6cd72895f803ad0186ee232c35
Alejandro Gutierrez Chavarria <113729680+adawa111@users.noreply.github.com>
2023-05-31 14:58:08 -0500

    ejercicio 1

--- src/main/java/com/example/lab7/controller/UsuariosController.java
+++ src/main/java/com/example/lab7/controller/UsuariosController.java
@@ -3,13 +3,15 @@ package com.example.lab7.controller;
 
 import com.example.lab7.Entity.Usuarios;
 import com.example.lab7.repository.UsuariosRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -25,5 +27,32 @@ public class UsuariosController {
     public List<Usuarios> listaUsuarios(){
         return usuariosRepository.findAll();
     }
+
+    @PostMapping("/saveUser")
+    public ResponseEntity<HashMap<String, Object>> saveUser(
+            @RequestBody Usuarios usuarios,
+            @RequestParam(value = "fetchId", required = false) boolean fetchId) {
+
+        HashMap<String, Object> responseJson = new HashMap<>();
+        if(fetchId){
+            responseJson.put("id",usuarios.getId());
+        }
+        usuariosRepository.save(usuarios);
+        responseJson.put("estado","creado");
+        responseJson.put("id creado", usuarios.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
+
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<HashMap<String,String>> gestionException(HttpServletRequest request){
+        HashMap<String,String> responseMap = new HashMap<>();
+        if(request.getMethod().equals("POST")){
+            responseMap.put("estado","error");
+            responseMap.put("msg","Debe enviar un producto");
+        }
+        return ResponseEntity.badRequest().body(responseMap);
+    }
+
 
 }
